53177e3ebddf8a7d8e2c35f285bca2ca3d2d6622
raa99099 <aura.ramadhani@mhs.unsoed.ac.id>
2026-06-20 16:46:11 +0700

    Create ExploreView component to display images
    Fetch images from API and display in gallery.

---
+++ src/views/ExploreView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref,onMounted } from 'vue'
+
+const images = ref([])
+
+onMounted(async()=>{
+  const response = await fetch('http://localhost:3000/api/explore')
+  images.value = await response.json()
+})
+</script>
+
+<template>
+<div class="container">
+<h1>Explore</h1>
+
+<div class="gallery">
+<div class="card" v-for="img in images" :key="img.id">
+<img :src="img.image_url">
+<p>{{ img.title }}</p>
+</div>
+</div>
+
+</div>
+</template>
